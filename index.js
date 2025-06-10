b75d335b540ce982cec2878a7baa3094eb573000
MorrisKM <itsmorrismunene@gmail.com>
2025-06-10 19:19:12 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+}
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newBest) {
+    bestCustomer = newBest
+}
+const leastFavoriteCustomer = 'ruth';
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'neil'
+}
+
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
+
